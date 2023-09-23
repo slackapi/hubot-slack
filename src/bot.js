305f17e2d96d0ef62304bec1575f80be63498458
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2023-09-24 00:50:53 +0200

    Fix profile problem (#28)
    * fix: profile problem
    * Remove unnecessary call to conversion info.

--- src/bot.js
+++ src/bot.js
@@ -457,7 +457,11 @@ class SlackBot extends Adapter {
     // Hubot expects all user objects to have a room property that is used in the envelope for the message after it
     // is received
     from.room = channel ?? '';
-    from.name = from.profile.display_name;
+    
+    // looks like sometimes the profile is not set
+    if (from.profile) {
+      from.name = from.profile.display_name;
+    }
 
     // add the bot id to the message if it's a direct message
     message.body.event.text = this.addBotIdToMessage(message.body.event);
